ed7b7c7d6d93210844585731d7bf39480fda77ac
Aleksandr Tretyakov <flonednb@gmail.com>
2022-09-30 21:40:55 +0500

    Remove VS files and move to CMake, plus run formatting.
    Here is a summary of this commit:
    - remove Visual Studio related files,
    - improve README,
    - use CMake,
    - reorganize directories and files,
    - add `clang-format` and format files,
    - fix 2 `clang-tidy` warnings in `dumpStats`,
    - add `getStats` function that is similar to `dumpStats` but has less
    information and returns a string instead of printing everything.

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # TGC2
 
-## A Tiny, precise, generational, mark & sweep, Garbage Collector for C++.
+A tiny, precise, generational, mark & sweep, Garbage Collector for C++.
 
-参考请注明出处，谢谢。
+Fork of [crazybie/tgc2](https://github.com/crazybie/tgc2).
 
-### Motivation
+# Motivation
 - Scenarios that shared_ptr can't solve, e.g. object dependencies are dynamically constructed with no chance to recognize the usage of shared & weak pointers.
 - Try to make things simpler compared to shared_ptr and Oilpan, e.g. networking programs using callbacks for async io operations heavily.     
 - A very good experiment to design a GC dedicated to the C++ language and see how the language features can help.    
 
-### Highlights
+# Highlights
 - Non-intrusive
     - Use like shared_ptr.
     - Do not need to replace the global new operator.
@@ -29,42 +29,92 @@
 - Precise.
     - Ensure no memory leaks as long as objects are correctly traced.
 
-### Improvements compared with tgc
+# Improvements compared to tgc1
 - better throughputs by using generational algorithm
 - gc pointers can be global variables
 - fast gc pointer construction
 - support continuous vector
 - unified way to handle class and containers
 - gc pointer has smaller size
-- do not support multi-inheritance
-- can customize the condition of full collection
+- does not support multiple inheritance
+- can customize the condition of a full garbage collection
 
+# Internals
+- This collector uses the triple color, mark & sweep algorithm internally.    
+- Pointers are constructed as roots by default unless detected as children of other object.
+- A GC pointer is with the size of 3-pointers:
+    - one flag determin whether it's root or not.
+    - an index for fast unregistering from collector.
+    - one raw pointer to the object and another one raw pointer to the correspoinding meta-object, this is to support:
+        - multiple inheritance.
+        - pointer to fields of other object, aka internal pointer.
+- Every class has a global meta-object keeping the necessary meta-information (e.g. class size and offsets of member pointers) used by GC, so programs using lambdas heavily may have some memory overhead. Besides, as the initialization order of global objects is not well defined, you should not use GC pointers as global variables too (there is an assert checking it).
+- Construct & copy & modify GC pointers are slower than shared_ptr, much slower than raw pointers(Boehm GC).
+    - Every GC pointer must register itself to the collector and unregister on destruction as well.
+    - Since C++ does not support ref-qualified constructors, the gc_new returns a temporary GC pointer bringing in some meaningless overhead. Instead, using gc_new_meta can bypass the construction of the temporary making things a bit faster.
+    - Member pointers offsets of one class are calculated and recorded at the first time of creating the instance of that class.
+    - Modifying a GC pointer will trigger a GC color adjustment which may not be cheap as well.
+- Each allocation has a few extra space overhead (size of two pointers at most), which is used for memory tracing.
+- Marking & swapping should be much faster than Boehm GC, due to the deterministic pointer management, no scanning inside the memories at all, just iterating pointers registered in the GC.
+- To make objects in proper tracing chain, you must use GC wrappers of STL containers instead, otherwise, memory leaks may occur.
+- gc_vector stores pointers of elements making its storage not continuous as a standard vector, this is necessary for the GC. All wrapped containers of STL stores GC pointers as elements.
+- You can manually call gc_delete to trigger the destructor of an object and let the GC claim the memory automatically. Besides, double free is also safe.
+- For the multi-threaded version, the collection function should be invoked from the main thread therefore the destructors can be triggered in the main thread as well.
 
+# Performance Advice
+- Performance is not the first goal of this library. 
+    - Results from tests, a simple allocation of an integer is about 8~10 slower than standard new(see test), so benchmark your program if GC pointers are heavily used in the performance-critical parts(e.g. VM of another language).
+    - Use the references to GC pointers as much as possible. (e.g. function parameters, see internals section)
+    - Use gc_new_array to get a collectible continuous array for better performance in some special cases (see internals section).
+    - Continuous efforts will be put to optimize the performance at a later time.
+    - Languages with GC built-in prefer to create a huge number of heap objects which will give large pressure to the GC, some languages even use pointer escaping analyzing algorithm to increase the recycling efficiency, but it's not a serious problem to C++ as it has RAII and does not use heap objects everywhere. So the throughput of this triple-color GC should be efficient enough. 
+- For real-time applications:
+    - Static strategy: just call gc_collect with a suitable step count regularly in each frame of the event loop.
+    - Dynamic strategy: you can specify a small step count(default is 255) for one collecting call and time it to see if still has time left to collect again, otherwise do collecting at the next time.    
+- As memories are managed by GC, you can not release them immediately. If you want to get rid of the risk of OOM on some resource-limited system, memories guaranteed to have no pointers in it can be managed by shared_ptrs or raw pointers.
+- The single-threaded version(by default) should be much faster than the multi-threaded version because no locks are required at all. Please define TGC_MULTI_THREADED to enable the multi-threaded version.
 
-### Internals
+# Setup (Build)
 
-### Performance Advice
+Prerequisites:
+- compiler that supports C++17
+- [CMake](https://cmake.org/download/)
+- [Doxygen](https://doxygen.nl/download.html)
 
-### Usage
+First, clone this repository:
 
-Please see the tests in 'test.cpp'.
+```
+git clone https://github.com/Flone-dnb/tgc2.git
+cd tgc2
+git submodule update --init --recursive
+```
 
-### Refs
+Then, if you've never used CMake before:
+
+Create a `build` directory next to this file, open created `build` directory and type `cmd` in Explorer's address bar. This will open up a console in which you need to type this:
+
+```
+cmake -DCMAKE_BUILD_TYPE=Debug .. // for debug mode
+cmake -DCMAKE_BUILD_TYPE=Release .. // for release mode
+```
+
+This will generate project files that you will use for development.
+
+# Update
+
+To update this repository:
+
+```
+git pull
+git submodule update --init --recursive
+```
+
+# Usage
+
+Please see the tests in `tests/test.cpp`.
+
+# References
 
 - https://www.codeproject.com/Articles/938/A-garbage-collection-framework-for-C-Part-II.
 - Boehn GC: https://github.com/ivmai/bdwgc/
 - Oilpan GC: https://chromium.googlesource.com/chromium/src/+/master/third_party/blink/renderer/platform/heap/BlinkGCDesign.md#Threading-model
-
-### License
-
-The MIT License
-
-```
-Copyright (C) 2018 soniced@sina.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-```
---
+++ src/tgc2.cpp
@@ -0,0 +1,404 @@
+#include "tgc2.h"
+
+#include <algorithm>
+
+#ifdef _WIN32
+#include <crtdbg.h>
+#endif
+
+namespace tgc2 {
+    namespace details {
+
+        int ClassMeta::isCreatingObj = 0;
+        ClassMeta::Alloc ClassMeta::alloc = nullptr;
+        ClassMeta::Dealloc ClassMeta::dealloc = nullptr;
+        Collector* Collector::inst = nullptr;
+        vector<char*> IPtrEnumerator::buf;
+
+        //////////////////////////////////////////////////////////////////////////
+
+        template <typename C> void vector_remove(C& c, typename C::value_type& v) {
+            c.erase(remove(c.begin(), c.end(), v), c.end());
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+
+        void ObjMeta::destroy() {
+            if (!arrayLength)
+                return;
+            klass->memHandler(klass, ClassMeta::MemRequest::Dctor, objPtr(), arrayLength);
+            arrayLength = 0;
+        }
+
+        void ObjMeta::operator delete(void* p) {
+            auto* m = (ObjMeta*)p;
+            m->klass->callDealloc(m);
+        }
+
+        bool ObjMeta::containsPtr(char* p) {
+            auto* o = objPtr();
+            return o <= p && p < o + klass->size * arrayLength;
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+
+        PtrBase* ObjPtrEnumerator::getNext() {
+            assert(klass->registered);
+            if (auto* subPtrs = klass->subPtrOffsets) {
+                if (arrayElemIdx < len && subPtrIdx < subPtrs->size()) {
+                    auto* obj = base + arrayElemIdx * klass->size;
+                    auto* subPtr = obj + (*klass->subPtrOffsets)[subPtrIdx];
+                    if (++subPtrIdx >= klass->subPtrOffsets->size()) {
+                        arrayElemIdx++;
+                        subPtrIdx = 0;
+                    }
+                    return (PtrBase*)subPtr;
+                }
+            }
+            return nullptr;
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+
+        PtrBase::PtrBase() : isRoot(true), isOld(false) {
+            auto* c = Collector::inst ? Collector::inst : Collector::get();
+            c->tryRegisterToClass(this);
+        }
+
+        PtrBase::PtrBase(void* obj) : isRoot(true), isOld(false) {
+            auto* c = Collector::inst ? Collector::inst : Collector::get();
+            c->tryRegisterToClass(this);
+            meta = c->globalFindOwnerMeta(obj);
+            writeBarrier();
+        }
+
+        PtrBase::~PtrBase() {
+            auto* c = Collector::inst;
+            c->unrefs.emplace_back(this);
+        }
+
+        void PtrBase::writeBarrier() {
+            if (this->meta)
+                Collector::inst->delayIntergenerationalPtrs.insert(this);
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+
+        ObjMeta* ClassMeta::newMeta(size_t cnt) {
+            auto* c = Collector::inst ? Collector::inst : Collector::get();
+
+            if (c->gcCond && c->gcCond->needMinorGc(c))
+                c->collect();
+
+            ObjMeta* meta = nullptr;
+            try {
+                isCreatingObj++;
+                auto* p = callAlloc(size * cnt + sizeof(ObjMeta));
+                meta = new (p) ObjMeta(this, p + sizeof(ObjMeta), cnt);
+                // Allow using gc_from(this) in the constructor of the creating object.
+                c->addMeta(meta);
+                return meta;
+            } catch (std::bad_alloc&) {
+                if (meta)
+                    callDealloc(meta);
+                throw;
+            }
+        }
+
+        void ClassMeta::endNewMeta(ObjMeta* meta, bool failed) {
+            auto* c = Collector::inst;
+            isCreatingObj--;
+            vector_remove(c->creatingObjs, meta);
+            if (failed) {
+                c->newGen.remove(meta);
+                callDealloc(meta);
+            } else {
+                meta->klass->registered = true;
+            }
+        }
+
+        void ClassMeta::registerSubPtr(ObjMeta* owner, PtrBase* p) {
+            auto offset = (OffsetType)((char*)p - owner->objPtr());
+            if (!subPtrOffsets) {
+                subPtrOffsets = new vector<OffsetType>();
+                subPtrOffsets->push_back(offset);
+            } else if (offset > subPtrOffsets->back()) { // constructor recursived
+                subPtrOffsets->push_back(offset);
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+
+        Collector* Collector::get() {
+            if (!inst) {
+#ifdef _WIN32
+                _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+                inst = new Collector();
+                atexit([] { delete inst; });
+            }
+            return inst;
+        }
+
+        Collector::Collector() {
+            roots.reserve(1024 * 10);
+            unrefs.reserve(1024 * 10);
+            temp.reserve(1024 * 10);
+            intergenerationalPtrs.reserve(1024 * 10);
+            delayIntergenerationalPtrs.reserve(1024 * 10);
+            setGcCondition(new GcCondition_Time);
+        }
+
+        Collector::~Collector() {
+            while (newGen.size()) {
+                auto i = newGen.back();
+                newGen.pop_back();
+                delete i;
+            }
+            while (oldGen.size()) {
+                auto i = oldGen.back();
+                oldGen.pop_back();
+                delete i;
+            }
+            for (auto* i : IPtrEnumerator::buf)
+                delete[] i;
+
+            delete gcCond;
+        }
+
+        void Collector::addMeta(ObjMeta* meta) {
+            newGen.push_back(meta);
+            creatingObjs.push_back(meta);
+        }
+
+        void Collector::tryRegisterToClass(PtrBase* p) {
+            if (ClassMeta::isCreatingObj > 0) {
+                // owner may not be the current one(e.g. constructor recursed)
+                for (auto i = creatingObjs.rbegin(); i != creatingObjs.rend(); ++i) {
+                    auto* owner = *i;
+                    if (!owner->klass->registered && owner->containsPtr((char*)p)) {
+                        owner->klass->registerSubPtr(owner, p);
+                        break;
+                    }
+                }
+            }
+        }
+
+        void Collector::handleUnrefs() {
+            for (auto ptr : unrefs) {
+                intergenerationalPtrs.erase(ptr);
+                delayIntergenerationalPtrs.erase(ptr);
+                roots.erase(ptr);
+            }
+            unrefs.clear();
+        }
+
+        void Collector::handleDelayIntergenerationalPtrs() {
+            for (auto* p : delayIntergenerationalPtrs) {
+                if (p->isRoot)
+                    roots.insert(p);
+                else if (p->isOld) {
+                    intergenerationalPtrs.insert(p);
+                }
+            }
+            delayIntergenerationalPtrs.clear();
+        }
+
+        ObjMeta* Collector::globalFindOwnerMeta(void* obj) {
+            auto* meta = (ObjMeta*)((char*)obj - sizeof(ObjMeta));
+            if (meta->magic == ObjMeta::Magic)
+                return meta;
+            else
+                return nullptr;
+        }
+
+        void Collector::mark(ObjMeta* meta) {
+            auto doMark = [&](ObjMeta* meta) {
+                if (meta->color == ObjMeta::Color::White) {
+                    meta->color = ObjMeta::Color::Black;
+
+                    if (auto* ptrIt = meta->klass->enumPtrs(meta)) {
+                        for (; auto* child = ptrIt->getNext();) {
+                            if (auto* m = child->meta) {
+                                if (m->color == ObjMeta::Color::White)
+                                    temp.push_back(m);
+                            }
+                        }
+                        delete ptrIt;
+                    }
+                }
+            };
+
+            doMark(meta);
+            while (temp.size()) {
+                auto* m = temp.back();
+                temp.pop_back();
+                doMark(m);
+            }
+        }
+
+        // Unified way for objects and containers.
+        void Collector::preMark(ObjMeta* meta) {
+            auto work = [&](ObjMeta* meta) {
+                // fix for circular references.
+                if (meta->color == ObjMeta::Color::Black) {
+                    // sweep function cannot reset color of intergenerational objects.
+                    meta->color = ObjMeta::Color::White;
+
+                    meta->hasSubPtrs = true;
+                    if (auto* it = meta->klass->enumPtrs(meta)) {
+                        meta->hasSubPtrs = false;
+
+                        for (; auto* ptr = it->getNext();) {
+                            ptr->isRoot = false;
+                            meta->hasSubPtrs = true;
+
+                            if (auto* subMeta = ptr->meta) {
+                                // fix for circular references.
+                                if (subMeta->color == ObjMeta::Color::Black)
+                                    temp.push_back(ptr->meta);
+                            }
+                        }
+                        delete it;
+                    }
+                }
+            };
+
+            work(meta);
+            while (temp.size()) {
+                auto* m = temp.back();
+                temp.pop_back();
+                work(m);
+            }
+        }
+
+        void Collector::minorCollect() {
+            freeObjCntOfPrevGc = 0;
+            newGenGcCount++;
+
+            for (auto meta : newGen)
+                preMark(meta);
+
+            handleUnrefs();
+            handleDelayIntergenerationalPtrs();
+
+            for (auto ptr : roots) {
+                if (ptr->meta && !ptr->isOld) {
+                    mark(ptr->meta);
+                }
+            }
+
+            for (auto ptr : intergenerationalPtrs) {
+                if (ptr->meta)
+                    mark(ptr->meta);
+            }
+
+            sweep(newGen);
+        }
+
+        void Collector::sweep(MetaSet& gen) {
+            for (auto it = gen.begin(); it != gen.end();) {
+                auto* meta = *it;
+
+                if (meta->color == ObjMeta::Color::White) {
+                    freeObjCntOfPrevGc++;
+                    it = gen.erase(it);
+                    delete meta;
+                } else {
+                    if (!full && ++meta->scanCountInNewGen >= scanCountToOldGen) {
+                        meta->scanCountInNewGen = 0;
+                        it = newGen.erase(it);
+                        promote(meta);
+                    } else
+                        ++it;
+                }
+            }
+
+            if (trace)
+                printf("sweep %s, free cnt:%d\n", &gen == &oldGen ? "old" : "new", freeObjCntOfPrevGc);
+        }
+
+        void Collector::promote(ObjMeta* meta) {
+            oldGen.push_back(meta);
+            if (auto it = meta->klass->enumPtrs(meta)) {
+                for (; auto* p = it->getNext();) {
+                    p->isOld = true;
+                    if (p->meta)
+                        intergenerationalPtrs.insert(p);
+                }
+                delete it;
+            }
+        }
+
+        void Collector::fullCollect() {
+            freeObjCntOfPrevGc = 0;
+            full = true;
+            fullGcCount++;
+
+            for (auto meta : newGen)
+                preMark(meta);
+            for (auto meta : oldGen)
+                preMark(meta);
+
+            handleUnrefs();
+            handleDelayIntergenerationalPtrs();
+
+            for (auto ptr : roots) {
+                if (ptr->meta) {
+                    mark(ptr->meta);
+                }
+            }
+
+            sweep(newGen);
+            sweep(oldGen);
+            full = false;
+        }
+
+        void Collector::collect() {
+            if (gcCond && gcCond->needFullGc(this)) {
+                fullCollect();
+            } else {
+                minorCollect();
+            }
+        }
+
+        void Collector::dumpStats() {
+            printf("========= [gc] ========\n");
+            printf("[newGen meta    ] %llu\n", newGen.size());
+            printf("[oldGen meta    ] %llu\n", oldGen.size());
+            auto liveCnt = 0;
+            for (auto i : newGen)
+                if (i->arrayLength)
+                    liveCnt++;
+            for (auto i : oldGen)
+                if (i->arrayLength)
+                    liveCnt++;
+            printf("[live objects   ] %3d\n", liveCnt);
+            printf("[new gen gc cnt ] %3d\n", newGenGcCount);
+            printf("[full gc cnt    ] %3d\n", fullGcCount);
+            printf("[last freed objs] %3d\n", freeObjCntOfPrevGc);
+            printf("=======================\n");
+        }
+
+        string Collector::getStats() {
+            std::string sOutput = "========= [Garbage Collector] ========\n";
+
+            // Count how much alive objects right now.
+            size_t iAliveObjectsCount = 0;
+            for (auto i : newGen)
+                if (i->arrayLength)
+                    iAliveObjectsCount += 1;
+            for (auto i : oldGen)
+                if (i->arrayLength)
+                    iAliveObjectsCount += 1;
+
+            // Add to output.
+            sOutput += "[alive objects   ]: " + std::to_string(iAliveObjectsCount) + "\n";
+            sOutput += "[last freed count]: " + std::to_string(freeObjCntOfPrevGc) + "\n";
+
+            return sOutput;
+        }
+
+    } // namespace details
+} // namespace tgc2
